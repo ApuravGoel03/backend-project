d8aea4ae20202665fba83127ede7a954a83baab9
apuravgoel14 <apuravgoel786@gmail.com>
2024-08-24 19:47:18 +0530

    Minor changes in async handler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@ const asyncHandler = (responseHandler) =>{
     return async (req,res,next) =>{
         try {
             await responseHandler(req,res,next);
-        } catch (error) {
+        } catch (err) {
             res.status(err.code || 500).json({
                 success: false,
                 message: err.message
